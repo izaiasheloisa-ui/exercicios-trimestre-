7e73985afe9971fc9c60968f7faed778cceee36a
Heloisa Eurich de Souza <izaias.heloisa@escola.pr.gov.br>
2026-09-24 11:48:03 -0300

    Delete KOTLIN directory

--- KOTLIN/ex01.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-println("=====================")
-println("MEU PRIMEIRO PROGRAMA")
-println("======================)
-println("Nome: Aluno Exemplo")
-println("Turma: 3DS")
-println("Escola: Alberto Gomes Veiga")
-println("Bem-vindo ao Kotlin!")
-}
